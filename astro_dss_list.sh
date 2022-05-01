007095774d797eee6b55d4900963c5f0f124cc57
Chris Nurse <45703746+chrisnurse@users.noreply.github.com>
2022-05-01 12:57:39 +0800

    Initial commit

---
+++ astro_dss_list.sh
@@ -0,0 +1,55 @@
+echo "DSS file list" 
+echo "CHECKED	TYPE	FILE" 
+
+for img in $(find $1 -name $2)
+do 
+    echo "1	light	$img"  
+done
+
+if ! [ "$3" = "" ]
+then
+    echo "0	reflight	$3"  
+fi
+
+if [ "$4" = "cal" ]
+then
+    if [ "$5" = "" ]
+    then
+        echo "When using calibration, you must specific Bin1, Bin2, etc... "
+        exit 1
+    fi
+
+    if [ -d "$BIAS_PATH" ]
+    then
+        for img in $(find $BIAS_PATH -name "*$5*.fit")
+        do 
+            echo "1	bias	$img"  
+        done
+    fi
+
+    if [ -d "$DARKS_PATH" ]
+    then
+        for img in $(find $DARKS_PATH -name "**$5*.fit")
+        do 
+            echo "1	dark	$img"  
+        done
+    fi
+
+    if [ -d "$DARK_FLATS_PATH" ]
+    then
+        for img in $(find $DARK_FLATS_PATH -name "**$5*.fit")
+        do 
+            echo "1	darkflat	$img"  
+        done
+    fi
+
+    if [ -d "$FLATS_PATH" ]
+    then
+        for img in $(find $FLATS_PATH -name "**$5*.fit")
+        do 
+            echo "1	flat	$img"  
+        done
+    fi
+fi
+
+cat $ASTRO_HOME/scripts/dss_prefs.txt 
---
+++ astro_dss_prep.sh
@@ -0,0 +1,57 @@
+if [ "$1" = "" ]
+then
+    echo "Usage:"
+    echo "  astro_dss_prep.sh Bin"
+    echo
+    echo "Arguments:"
+    echo "  Bin: Bin1, Bin2, etc. Must match the part of the file name that specifies binning."
+    exit 1
+fi
+
+BIN=$1
+
+echo "Creating Deep Sky Stacker file list in ./dss_images.txt"
+echo
+
+astro_dss_list.sh . *.fit > dss_images.txt
+
+echo "Registering images"
+echo "This can take a while and you may want to review the Deep Sky Stacker preferences"
+echo
+echo "Deep Sky Stacker is registering and scoring the images..."
+
+DeepSkyStackerCL.exe /r dss_images.txt > /dev/null
+
+echo "Deep Sky Stacker has registered the images"
+echo
+
+echo "Allocating the highest scoring image as the reference frame..."
+echo
+
+BEST_IMAGE=""
+BEST_SCORE=0
+
+for score_file in $(find . -name "*.Info.txt")
+do 
+    SCORE=$(cat $score_file | tr "\r\n" "\n" | grep "OverallQuality" | sed "s/OverallQuality = //" | sed "s/\.//")
+
+#    echo "** $score_file: $SCORE **"
+
+    if [ "$SCORE" -gt "$BEST_SCORE" ]
+    then
+        BEST_IMAGE=$(echo "$score_file" | sed "s/.Info.txt/.fit/")
+        BEST_SCORE=$SCORE
+    fi
+    
+done
+
+echo "Best image is: $BEST_IMAGE"
+echo "        Score: $BEST_SCORE"
+
+[ -d ./L ] && astro_dss_list.sh ./L *.fit $BEST_IMAGE cal $BIN > l_dss_list.txt && 
+[ -d ./R ] && astro_dss_list.sh ./R *.fit $BEST_IMAGE cal $BIN > r_dss_list.txt
+[ -d ./G ] && astro_dss_list.sh ./G *.fit $BEST_IMAGE cal $BIN > g_dss_list.txt
+[ -d ./B ] && astro_dss_list.sh ./B *.fit $BEST_IMAGE cal $BIN > b_dss_list.txt
+[ -d ./H ] && astro_dss_list.sh ./H *.fit $BEST_IMAGE cal $BIN > h_dss_list.txt
+[ -d ./O ] && astro_dss_list.sh ./O *.fit $BEST_IMAGE cal $BIN > o_dss_list.txt
+[ -d ./S ] && astro_dss_list.sh ./S *.fit $BEST_IMAGE cal $BIN > s_dss_list.txt
